8a1c3892c4fdb29597ff38cee97c8a0fa7d4b6a3
Israel <israelmatiasl@gmail.com>
2025-10-31 21:48:26 -0500

    feat: enable cors to comunicate with client

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -12,7 +12,17 @@ func main() {
 	service := application.NewShortestPathService()
 	handler := handlers.NewHTTPHandler(service)
 
-	http.HandleFunc("/shortest-path", handler.ShortestPath)
+	//http.HandleFunc("/shortest-path", handler.ShortestPath)
+
+	http.HandleFunc("/shortest-path", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		if r.Method == "OPTIONS" {
+			return
+		}
+		handler.ShortestPath(w, r)
+	})
 
 	fmt.Println("Servidor ejecutándose en http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
